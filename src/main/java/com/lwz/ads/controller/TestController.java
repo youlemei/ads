e9d3b63fb6fff7febe4721938c9725f84903df1b
liweizhou <liweizhou@yy.com>
2020-02-14 00:29:12 +0800

    profile

--- src/main/java/com/lwz/ads/controller/TestController.java
+++ src/main/java/com/lwz/ads/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.lwz.ads.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ import java.util.Map;
  * @author liweizhou 2020/2/10
  */
 @Slf4j
-@ConditionalOnProperty(name = "spring.profiles.active", havingValue = "dev")
+@Profile("dev")
 @RestController
 public class TestController {
 
