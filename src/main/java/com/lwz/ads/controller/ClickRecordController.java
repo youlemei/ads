84c59a53f655ccef99a860272e095aee54a44fa4
liweizhou <liweizhou@yy.com>
2020-08-17 11:56:16 +0800

    点击记录

--- src/main/java/com/lwz/ads/controller/ClickRecordController.java
+++ src/main/java/com/lwz/ads/controller/ClickRecordController.java
@@ -1,8 +1,11 @@
 package com.lwz.ads.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lwz.ads.bean.PageResponse;
+import com.lwz.ads.constant.Const;
 import com.lwz.ads.mapper.entity.ClickRecord;
 import com.lwz.ads.service.impl.ClickRecordServiceImpl;
 import com.lwz.ads.util.DateUtils;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author liweizhou 2020/6/8
@@ -30,10 +35,10 @@ public class ClickRecordController {
     private ClickRecordServiceImpl clickRecordService;
 
     @GetMapping
-    public PageResponse<ClickRecord> list(Long adId, Long channelId, String mac, String date,
-                                         HttpServletRequest request,
-                                         @RequestParam(defaultValue = "1") long pageIndex,
-                                         @RequestParam(defaultValue = "10") long pageSize) {
+    public PageResponse<JSONObject> list(Long adId, Long channelId, String mac, String date,
+                                   HttpServletRequest request,
+                                   @RequestParam(defaultValue = "1") long pageIndex,
+                                   @RequestParam(defaultValue = "10") long pageSize) {
         String ip = IPUtils.getRealIp(request);
         log.info("list adId:{} channelId:{} mac:{} date:{} ip:{}", adId, channelId, mac, date, ip);
 
@@ -48,7 +53,19 @@ public class ClickRecordController {
         }
         Page<ClickRecord> page = new Page<>(pageIndex, pageSize);
         IPage<ClickRecord> pageInfo = clickRecordService.getBaseMapper().selectPageWithDate(page, adId, channelId, mac, date);
-        return PageResponse.success(pageInfo.getCurrent(), pageInfo.getSize(), pageInfo.getTotal(), pageInfo.getRecords());
+        List<JSONObject> list = pageInfo.getRecords().stream().map(clickRecord -> {
+            JSONObject item = new JSONObject();
+            item.put("id", clickRecord.getId());
+            item.put("adId", clickRecord.getAdId());
+            item.put("channelId", clickRecord.getChannelId());
+            item.put("createTime", clickRecord.getCreateTime());
+            item.put("mac", clickRecord.getMac());
+            item.put("ip", clickRecord.getIp());
+            item.put("callback", JSON.parseObject(clickRecord.getParamJson()).get(Const.CALLBACK));
+            item.put("clickStatus", clickRecord.getClickStatus());
+            return item;
+        }).collect(Collectors.toList());
+        return PageResponse.success(pageInfo.getCurrent(), pageInfo.getSize(), pageInfo.getTotal(), list);
     }
 
 }
